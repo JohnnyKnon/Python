df978b7ba40c911b72190dc97a6a0563bf5394f2
JohnnyHong <86140440+JohnnyKnon@users.noreply.github.com>
2021-06-26 07:13:47 +0000

    조건문 if, else, if not

--- main.py
+++ main.py
@@ -41,3 +41,25 @@ y = False
 print(e)
 print(y)
 
+# 조건문
+
+if 2 > 1: 
+  print("Hello")
+
+if 1 > 2:
+  print("Hello") # 1은 2보다 크지 않으므로 else인 Im not hello 송출
+else:
+  print("Im not hello")
+
+# not 을 사용한 예시
+
+if not 1 > 2:
+  print("YES!!")
+
+# not 과 else
+
+if not 2 > 1:
+  print("YES!!")
+else:
+  print("NO!!")
+  
